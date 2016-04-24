b9bbabfcd62fe0d1525784b6b84c7f0a99346bff
Rizwan Huq <rizwanhuq@gmail.com>
2016-04-24 13:06:40 -0400

    Adding ex39

---
+++ ex39.py
@@ -0,0 +1,15 @@
+# creating a mapping of states to abbreviations
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI',
+}
+
+# create a basic set of states and some cities in them
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville',
+}
